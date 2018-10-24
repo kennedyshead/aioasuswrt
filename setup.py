8ccb4bd5e22e9f0aafc4eb44775277e258d00833
magnusknutas <magnus@thefarm.se>
2018-10-24 09:14:00 +0200

    Adding transfer information

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 __author__ = 'Magnus Knutas'
-VERSION = '1.0.1'
+VERSION = '1.1.0'
 
 setup(
     name='aioasuswrt',
--- test.py
+++ test.py
@@ -3,4 +3,17 @@ import asyncio
 
 conn = AsusWrt('192.168.1.1', 22, username='knutas', password='Freakdays123')
 
-asyncio.run(conn.async_get_packets_total())
+loop = asyncio.get_event_loop()
+
+
+async def printer(cmd):
+    data = await cmd()
+    print(data)
+
+loop.run_until_complete(printer(conn.async_get_packets_total))
+loop.run_until_complete(printer(conn.async_get_rx))
+loop.run_until_complete(printer(conn.async_get_tx))
+loop.run_until_complete(printer(conn.async_get_current_transfer_rates))
+loop.run_until_complete(printer(conn.async_get_current_transfer_rates))
+loop.run_until_complete(printer(conn.async_get_current_transfer_rates))
+
