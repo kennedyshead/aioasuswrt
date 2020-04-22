d109415a0c5d54b94cc9d92f5cf8fffa6cfeb79a
magnusknutas <magnus@thefarm.se>
2020-04-22 13:33:24 +0200

    Release 1.2.5
    Took 5 minutes

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 __author__ = 'Magnus Knutas'
-VERSION = '1.2.4'
+VERSION = '1.2.5'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
