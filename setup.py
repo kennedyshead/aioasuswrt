6e283ecb9e4178990cf0ca8b2ee89b6121609cc4
magnusknutas <magnus@thefarm.se>
2020-11-09 12:28:46 +0100

    Bumping version
    Took 5 minutes

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 __author__ = 'Magnus Knutas'
-VERSION = '1.2.8'
+VERSION = '1.3.0'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
