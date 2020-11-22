e76eaaffbaac222e168f79db6e331161019dec6d
Jorik de Vries <jorik.de.vries@gmail.com>
2020-11-22 17:43:55 +0100

    fix: Test on all supported python versions.

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -23,30 +23,36 @@ from .test_data import (
 )
 
 
-def mock_run_cmd(mocker, values):
-    iter = 0
+class MockRunCmd:
+    def __init__(
+        self,
+        mocker,
+        values,
+        patch_func="aioasuswrt.connection.SshConnection.async_run_command",
+    ):
+        self._values = values
+        self._iter = 0
 
-    async def patch_func(command, *args, **kwargs):
-        nonlocal iter
-        iter = iter + 1
+        # Now let's patch the actual function
+        mocker.patch(patch_func, side_effect=self._patch_func)
+
+    async def _patch_func(self, command, *args, **kwargs):
+        print(f"Called {command=}\nWith arguments {args=}, {kwargs=}")
+        self._iter = self._iter + 1
         try:
-            return values[iter - 1]
+            return self._values[self._iter - 1]
         except IndexError:
             print(
-                f"Not enough elements in return list! Iteration {iter} while list is {len(values)}."
+                f"Not enough elements in return list! Iteration {self._iter}"
+                f" while list is {len(self._values)}."
             )
             assert False
-
-    mocker.patch(
-        "aioasuswrt.connection.SshConnection.async_run_command",
-        side_effect=patch_func,
-    )
 
 
 @pytest.mark.asyncio
 async def test_get_wl(event_loop, mocker):
     """Testing wl."""
-    mock_run_cmd(mocker, [WL_DATA])
+    MockRunCmd(mocker, [WL_DATA])
     scanner = AsusWrt(host="localhost", port=22)
     devices = await scanner.async_get_wl()
     assert WL_DEVICES == devices
@@ -55,7 +61,7 @@ async def test_get_wl(event_loop, mocker):
 @pytest.mark.asyncio
 async def test_get_wl_empty(event_loop, mocker):
     """Testing wl."""
-    mock_run_cmd(mocker, [""])
+    MockRunCmd(mocker, [""])
     scanner = AsusWrt(host="localhost", port=22)
     devices = await scanner.async_get_wl()
     assert {} == devices
@@ -64,7 +70,7 @@ async def test_get_wl_empty(event_loop, mocker):
 @pytest.mark.asyncio
 async def test_async_get_leases(event_loop, mocker):
     """Testing leases."""
-    mock_run_cmd(mocker, [LEASES_DATA])
+    MockRunCmd(mocker, [LEASES_DATA])
     scanner = AsusWrt(host="localhost", port=22)
     data = await scanner.async_get_leases(NEIGH_DEVICES.copy())
     print(f"{data=}")
@@ -74,7 +80,7 @@ async def test_async_get_leases(event_loop, mocker):
 @pytest.mark.asyncio
 async def test_get_arp(event_loop, mocker):
     """Testing arp."""
-    mock_run_cmd(mocker, [ARP_DATA])
+    MockRunCmd(mocker, [ARP_DATA])
     scanner = AsusWrt(host="localhost", port=22)
     data = await scanner.async_get_arp()
     assert ARP_DEVICES == data
@@ -83,7 +89,7 @@ async def test_get_arp(event_loop, mocker):
 @pytest.mark.asyncio
 async def test_get_neigh(event_loop, mocker):
     """Testing neigh."""
-    mock_run_cmd(mocker, [NEIGH_DATA])
+    MockRunCmd(mocker, [NEIGH_DATA])
     scanner = AsusWrt(host="localhost", port=22)
     data = await scanner.async_get_neigh(NEIGH_DEVICES.copy())
     assert NEIGH_DEVICES == data
@@ -94,7 +100,7 @@ async def test_get_connected_devices_ap(event_loop, mocker):
     """Test for get asuswrt_data in ap mode."""
     # Note, unfortunately the order of data is important and should be the
     # same as in the `async_get_connected_devices` function.
-    mock_run_cmd(mocker, [WL_DATA, ARP_DATA, NEIGH_DATA, LEASES_DATA])
+    MockRunCmd(mocker, [WL_DATA, ARP_DATA, NEIGH_DATA, LEASES_DATA])
     scanner = AsusWrt(host="localhost", port=22, mode="ap", require_ip=True)
     data = await scanner.async_get_connected_devices()
     assert WAKE_DEVICES_AP == data
@@ -103,7 +109,7 @@ async def test_get_connected_devices_ap(event_loop, mocker):
 @pytest.mark.asyncio
 async def test_get_connected_devices_no_ip(event_loop, mocker):
     """Test for get asuswrt_data and not requiring ip."""
-    mock_run_cmd(mocker, [WL_DATA, ARP_DATA, NEIGH_DATA, LEASES_DATA])
+    MockRunCmd(mocker, [WL_DATA, ARP_DATA, NEIGH_DATA, LEASES_DATA])
     scanner = AsusWrt(host="localhost", port=22, mode="ap", require_ip=False)
     data = await scanner.async_get_connected_devices()
     assert WAKE_DEVICES_NO_IP == data
@@ -112,7 +118,7 @@ async def test_get_connected_devices_no_ip(event_loop, mocker):
 @pytest.mark.asyncio
 async def test_get_packets_total(event_loop, mocker):
     """Test getting packet totals."""
-    mock_run_cmd(mocker, [TX_DATA, RX_DATA])
+    MockRunCmd(mocker, [TX_DATA, RX_DATA])
     scanner = AsusWrt(host="localhost", port=22, mode="ap", require_ip=False)
     data = await scanner.async_get_tx()
     assert TX == data
@@ -123,7 +129,7 @@ async def test_get_packets_total(event_loop, mocker):
 @pytest.mark.asyncio
 async def test_async_get_temperature(event_loop, mocker):
     """Test getting temperature."""
-    mock_run_cmd(mocker, [TEMP_DATA])
+    MockRunCmd(mocker, [TEMP_DATA])
     scanner = AsusWrt(host="localhost", port=22, mode="ap", require_ip=False)
     data = await scanner.async_get_temperature()
     assert data == {"2.4GHz": 49.5, "5.0GHz": 54.5, "CPU": 77.0}
@@ -132,7 +138,7 @@ async def test_async_get_temperature(event_loop, mocker):
 @pytest.mark.asyncio
 async def test_async_get_loadavg(event_loop, mocker):
     """Test getting loadavg."""
-    mock_run_cmd(mocker, [LOADAVG_DATA])
+    MockRunCmd(mocker, [LOADAVG_DATA])
     scanner = AsusWrt(host="localhost", port=22, mode="ap", require_ip=False)
     data = await scanner.async_get_loadavg()
     assert data == [0.23, 0.5, 0.68]
@@ -141,7 +147,7 @@ async def test_async_get_loadavg(event_loop, mocker):
 @pytest.mark.asyncio
 async def test_async_get_interfaces_counts(event_loop, mocker):
     """Test getting loadavg."""
-    mock_run_cmd(mocker, [NETDEV_DATA])
+    MockRunCmd(mocker, [NETDEV_DATA])
     scanner = AsusWrt(host="localhost", port=22, mode="ap", require_ip=False)
     data = await scanner.async_get_interfaces_counts()
     assert data == INTERFACES_COUNT
